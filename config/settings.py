40622c450055927cf48f57a456fbd9e8327b56be
Gayratbek Akhmedov <gayratbekakhemdov@gmail.com>
2023-07-20 23:32:55 +0500

    asf

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ MEDIA_URL = '/media/'
                 # Add any other directories if necessary
 #                ]
 STATIC_ROOT = '/var/www/website/innomotor/static/'
-STATIC_URL = '/static/'
+STATIC_URL = '/var/www/website/innomotor/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
